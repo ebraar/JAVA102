263ce86fa5f6a45a3187de7615aae63c4610f0e2
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-11-30 15:52:31 +0300

    Add files via upload

---
+++ interface örnek kullanımı/IyzicoPaymentSystem.java
@@ -0,0 +1,17 @@
+public class IyzicoPaymentSystem implements PaymentProvider{
+
+    @Override
+    public boolean cancelCharge(int chargeId){
+        return false;
+    }
+
+    @Override
+    public int charge(double totalPrice){
+        return 0;
+    }
+
+    @Override
+    public String loadInvoice(int chargeId){
+        return null;
+    }
+}

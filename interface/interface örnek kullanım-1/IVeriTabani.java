38c9d1ddad906ca7ce2787f87395d89c7bc149b2
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-12-25 21:35:01 +0300

    Add files via upload

---
+++ interface/interface örnek kullanım-1/IVeriTabani.java
@@ -0,0 +1,11 @@
+public interface IVeriTabani {
+    void log();
+}
+/** inheritance ile interface bazı farklar dışında benzerdir.
+ * bir tür classtır fakat obje oluşturulmaz.
+ * içerisinde kesinlikle gövdeli metot bulunmaz.
+ * içerisinde değişken tanımlaması yapılmaz.
+ * bir sınıf birden fazla interface'i çağırabilir.
+ * bir interface'i birden fazla sınıf çağırabilir.
+ * intergace'in isimlendirmesinde kural gereği başta büyük ı harfi kullanılır.
+ */
---
+++ interface/interface örnek kullanım-1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        IVeriTabani iv1 = new Ogretmen();
+        IVeriTabani iv2 = new Ogrenci();
+
+        iv1.log();
+        iv2.log();
+    }
+}

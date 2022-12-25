81181a138ce260bbc8fe811fa3409ce93a54f979
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2022-12-25 20:35:24 +0200

    Delete interface/interface örnek kullanım-1 directory

--- interface/interface örnek kullanım-1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        IVeriTabani iv1 = new Ogretmen();
-        IVeriTabani iv2 = new Ogrenci();
-
-        iv1.log();
-        iv2.log();
-    }
-}
--- interface/interface örnek kullanım-1/Ogretmen.java
+++
@@ -1,6 +0,0 @@
-public class Ogretmen implements IVeriTabani{
-    @Override
-    public void log(){
-        System.out.println("Öğretmen veri tabanına eklendi.");
-    }
-}

aaf2c3fc97a7b35416d66b149d347a3486c777c6
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2023-01-01 17:14:00 +0300

    Add files via upload

---
+++ polymorphism example-2/Kedi.java
@@ -0,0 +1,10 @@
+public class Kedi extends Hayvan{
+    public Kedi(String isim){
+        super(isim);
+    }
+
+    @Override
+    public String konus(){
+        return this.getIsim() + " miyavlıyo..";
+    }
+}

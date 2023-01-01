2d27f91fcf76f2a7cbc3dfc966987b78bc7aed0f
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2023-01-01 17:12:36 +0300

    Add files via upload

---
+++ polymorphism example-2/At.java
@@ -0,0 +1,11 @@
+public class At extends Hayvan{
+
+    public At(String isim){
+        super(isim);
+    }
+
+    @Override
+    public String konus(){
+        return this.getIsim()+" kişniyor..";
+    }
+}

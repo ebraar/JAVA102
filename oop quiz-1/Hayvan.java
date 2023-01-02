8f9c2beaa0f5aaffdcd2de08c849532418f905b9
ebrar betül akgül <100383179+ebraar@users.noreply.github.com>
2023-01-03 00:08:01 +0300

    Add files via upload

---
+++ oop quiz-1/Hayvan.java
@@ -0,0 +1,10 @@
+public abstract class Hayvan {
+    int ayakSayisi;
+
+    public Hayvan(int ayakSayisi){
+        this.ayakSayisi = ayakSayisi;
+    }
+
+    public abstract void yuru();
+    public abstract void yemekYe();
+}

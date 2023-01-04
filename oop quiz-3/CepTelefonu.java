669eff3998e391fa96353724d7518538d29e4b7f
ebrar betül <100383179+ebraar@users.noreply.github.com>
2023-01-05 00:12:59 +0300

    Add files via upload

---
+++ oop quiz-3/CepTelefonu.java
@@ -0,0 +1,9 @@
+public class CepTelefonu {
+    void numarayiAra(String telNumarasi){
+        System.out.println(telNumarasi+" numarası aranıyor..");
+    }
+
+    void cagriSec(){
+        System.out.println("bağlanılıyor.. ");
+    }
+}

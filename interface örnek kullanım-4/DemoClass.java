ddfb7ba4f64fc691b7182a1b1b57d321ea39a94d
ebrar betül <100383179+ebraar@users.noreply.github.com>
2023-01-07 12:48:48 +0300

    Add files via upload

---
+++ interface örnek kullanım-4/DemoClass.java
@@ -0,0 +1,8 @@
+public class DemoClass implements FirstInterface,SecondInterface{
+    public void myMethod(){
+        System.out.println("some text..");
+    }
+    public void myOtherMethod(){
+        System.out.println("some other text..");
+    }
+}
